ef157bbb40bc38a47eb6a353dad9d053e62fa865
Daniel Ochoa <daniel@poeticsystems.com>
2015-03-29 15:12:29 -0500

    make tests pass for getter in object

--- test/index.js
+++ test/index.js
@@ -4,41 +4,27 @@ var test = require('tape');
 var subjectify = require('../lib/subjectify');
 
 test('subjectify', function(t) {
-  t.plan(5);
+  t.plan(3);
 
   t.ok(subjectify, 'exists');
   t.equal(typeof subjectify, 'function', 'is a function');
 
   t.test('get', function(st) {
-    st.plan(3);
+    st.plan(5);
 
-    var obj = {some: {deeply: {nested: {obj: { here: 'exists' }}}}};
-    var nestedAttr = subjectify(obj).get('some.deeply.nested.obj.here');
-    var undefinedAttr = subjectify(obj).get('some.lost.attr.not.found');
-    var nestedObj = subjectify(obj).get('some.deeply.nested');
+    var obj           = {some: {deeply: {nested: {obj: { here: 'exists' }}}}},
+        nestedAttr    = subjectify(obj).get('some.deeply.nested.obj.here'),
+        undefinedAttr = subjectify(obj).get('some.lost.attr.not.found'),
+        nestedObj     = subjectify(obj).get('some.deeply.nested'),
+        simplePath    = subjectify(obj).get('some'),
+        noResults     = subjectify(obj).get('lost');
 
     st.equal(nestedAttr, 'exists', 'returns the deeply nested attribute.');
     st.equal(undefinedAttr, undefined, 'returns undefined if not found.');
     st.deepEqual(nestedObj, obj.some.deeply.nested, 'returns nested object.');
+    st.deepEqual(simplePath, obj.some, 'returns first nested object.');
+    st.notOk(noResults, 'Single path not found.');
 
-  });
-
-  t.test('exists', function(st) {
-    st.plan(1);
-
-    var obj = {a: {deep: {nested: {thing: true}}}};
-    var nestedAttrExists = subjectify(obj).exists('a.deep.nested.thing');
-
-    st.ok(nestedAttrExists, 'should be truthy.');
-  });
-
-  t.test('set', function(st) {
-    st.plan(1);
-
-    var obj = {};
-    setProperty = subjectify(obj).set('a.very.nested.attr', true);
-
-    st.equal(obj.a.very.nested.attr, true, 'Set a very nested attribute in object.');
   });
 });
 
