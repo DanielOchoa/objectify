aca443946b99f8ab1e78a76c3220cbce12dd9afa
Daniel Ochoa <daniel@poeticsystems.com>
2015-03-29 22:05:40 -0500

    make setter for nested properties of object. remove dot at end of test assertion descriptions. move the method to build the array from the path to its town function.

--- test/index.js
+++ test/index.js
@@ -4,14 +4,14 @@ var test = require('tape');
 var subjectify = require('../lib/subjectify');
 
 test('subjectify', function(t) {
-  t.plan(5);
+  t.plan(6);
 
   t.ok(subjectify, 'exists');
   t.equal(typeof subjectify, 'function', 'is a function');
   t.throws(
     subjectify.bind(null, 'nonObject'),
     /You can only feed objects to subjectify./,
-    'Throws an error if a non-object is passed.'
+    'Throws an error if a non-object is passed'
   );
 
   t.test('get', function(st) {
@@ -24,12 +24,11 @@ test('subjectify', function(t) {
         simplePath    = subjectify(obj).get('some'),
         noResults     = subjectify(obj).get('lost');
 
-    st.equal(nestedAttr, 'exists', 'returns the deeply nested attribute.');
-    st.equal(undefinedAttr, undefined, 'returns undefined if not found.');
-    st.deepEqual(nestedObj, obj.some.deeply.nested, 'returns nested object.');
-    st.deepEqual(simplePath, obj.some, 'returns first nested object.');
-    st.notOk(noResults, 'Single path not found.');
-
+    st.equal(nestedAttr, 'exists', 'returns the deeply nested attribute');
+    st.equal(undefinedAttr, undefined, 'returns undefined if not found');
+    st.deepEqual(nestedObj, obj.some.deeply.nested, 'returns nested object');
+    st.deepEqual(simplePath, obj.some, 'returns first nested object');
+    st.notOk(noResults, 'Single path not found');
   });
 
   t.test('exists', function(st) {
@@ -41,25 +40,41 @@ test('subjectify', function(t) {
     var yetAnotherNestedAttr = subjectify(obj).exists('a');
     var falseNestedAttr  = subjectify(obj).exists('a.deep.wrong');
 
-    st.equal(nestedAttrExists, true, 'should be true.');
-    st.equal(anotherNestedAttr, true, 'should be true.');
+    st.equal(nestedAttrExists, true, 'should be true');
+    st.equal(anotherNestedAttr, true, 'should be true');
     st.equal(yetAnotherNestedAttr, true, 'should be true');
-    st.equal(falseNestedAttr, false, 'should be false.');
+    st.equal(falseNestedAttr, false, 'should be false');
 
     var falsyObj = {a: {nested: {falsy: false}}};
     var falsyAttr = subjectify(falsyObj).exists('a.nested.falsy');
 
-    st.equal(falsyAttr, false, 'should be false.');
+    st.equal(falsyAttr, false, 'should be false');
 
     var truthyObj = {a: {nested: {truthy: true}}};
     var truthyAttr = subjectify(truthyObj).exists('a.nested.truthy');
 
-    st.equal(truthyAttr, true, 'should be true.');
+    st.equal(truthyAttr, true, 'should be true');
 
     var undefinedExists = {a: {nested: {und: undefined}}};
     var undefinedAttr = subjectify(undefinedExists).exists('a.nested.und');
 
-    st.equal(undefinedAttr, false, 'should be false.');
+    st.equal(undefinedAttr, false, 'should be false');
+  });
+
+
+  t.test('set', function(st) {
+    st.plan(5);
+
+    var src = {};
+    var setProperty = subjectify(src).set('a.very.nested.attr', true);
+    st.equal(src.a.very.nested.attr, true, 'Set a very nested attribute in object');
+    st.equal(setProperty, true, 'should be true');
+
+    var context = 'iAmString';
+    var aString = subjectify(src).set('a.different.thing', context);
+    st.equal(src.a.different.thing, context, 'should be a string');
+    st.equal(aString, context)
+    st.ok(src.a.very.nested.attr, 'should be still set');
   });
 });
 
